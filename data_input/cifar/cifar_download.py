744cc063b820dbd82e2b02efbedaa3cf937245b3
赵磊 <bhneo@126.com>
2018-03-30 20:18:40 +0800

    update

--- data_input/cifar/cifar_download.py
+++ data_input/cifar/cifar_download.py
@@ -1,22 +1,34 @@
 import os
 import sys
-from six.moves import urllib
 import tarfile
+import tensorflow as tf
+
+from six.moves import urllib
+
+tf.app.flags.DEFINE_string('dir', '../../data/cifar10', 'directory')
+tf.app.flags.DEFINE_integer('cifar', 10, 'cifar 10 or 100')
+tf.app.flags.DEFINE_bool('b', True, 'is binary')
+FLAGS = tf.app.flags.FLAGS
 
 
 # 从网址下载数据集存放到data_dir指定的目录下
-CIFAR10_DATA_URL = 'http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz'
-CIFAR100_DATA_URL = 'http://www.cs.toronto.edu/~kriz/cifar-100-binary.tar.gz'
+CIFAR10_BINARY_URL = 'http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz'
+CIFAR100_BINARY_URL = 'http://www.cs.toronto.edu/~kriz/cifar-100-binary.tar.gz'
+CIFAR10_PYTHON_URL = 'http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
+CIFAR100_PYTHON_URL = 'http://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz'
 
 
 # 从网址下载数据集存放到data_dir指定的目录中
-def maybe_download_and_extract(data_dir, data_url=CIFAR10_DATA_URL):
+def maybe_download_and_extract(data_dir, cifar10or100=10, binary=True):
+    if cifar10or100 == 10:
+        URL = CIFAR10_BINARY_URL if binary else CIFAR10_PYTHON_URL
+    elif cifar10or100 == 100:
+        URL = CIFAR100_BINARY_URL if binary else CIFAR100_PYTHON_URL
     """下载并解压缩数据集 from Alex's website."""
     dest_directory = data_dir  # '../CIFAR10_dataset'
-    DATA_URL = data_url
     if not os.path.exists(dest_directory):
         os.makedirs(dest_directory)
-    filename = DATA_URL.split('/')[-1]  # 'cifar-10-binary.tar.gz'
+    filename = URL.split('/')[-1]  # 'cifar-10-binary.tar.gz'
     filepath = os.path.join(dest_directory, filename)  # '../CIFAR10_dataset\\cifar-10-binary.tar.gz'
     if not os.path.exists(filepath):
         def _progress(count, block_size, total_size):
@@ -24,16 +36,16 @@ def maybe_download_and_extract(data_dir, data_url=CIFAR10_DATA_URL):
                                                              float(count * block_size) / float(total_size) * 100.0))
             sys.stdout.flush()
 
-        filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+        filepath, _ = urllib.request.urlretrieve(URL, filepath, _progress)
         print()
         statinfo = os.stat(filepath)
         print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
 
-    # if data_url== CIFAR10_DATA_URL:
-    #     extracted_dir_path = os.path.join(dest_directory,'cifar-10-batches-bin')  # '../CIFAR10_dataset\\cifar-10-batches-bin'
-    # else :
-    #     extracted_dir_path = os.path.join(dest_directory, 'cifar-100-binary')  # '../CIFAR10_dataset\\cifar-10-batches-bin'
-    # if not os.path.exists(extracted_dir_path):
-    #     tarfile.open(filepath, 'r:gz').extractall(dest_directory)
-
     tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+
+
+def main(_):
+    maybe_download_and_extract(FLAGS.dir, FLAGS.cifar, FLAGS.b)
+
+if __name__ == "__main__":
+    tf.app.run()
--- data_input/cifar/cifar_input.py
+++ data_input/cifar/cifar_input.py
@@ -23,16 +23,12 @@ import os
 
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
+import numpy as np
 
-# Process images of this size. Note that this differs from the original CIFAR
-# image size of 32 x 32. If one alters this number, then the entire model
-# architecture will change and any model would need to be retrained.
-# IMAGE_SIZE = 24
 
 # Global constants describing the CIFAR-10 data set.
 
 # 用于描述CiFar数据集的全局常量
-# NUM_CLASSES = 10
 IMAGE_SIZE = 32
 IMAGE_DEPTH = 3
 NUM_CLASSES_CIFAR10 = 10
@@ -348,3 +344,132 @@ def inputs(cifar10or20or100, eval_data, data_dir, batch_size, distort=False):
     return _generate_image_and_label_batch(image, label,
                                            min_queue_examples, batch_size, height,
                                            shuffle=False if eval_data else True)
+
+
+### load data by python ###
+
+LABEL_SIZE = 1
+PIXEL_DEPTH = 255
+NUM_CLASSES = 10
+
+TRAIN_NUM = 10000
+TRAIN_NUMS = 50000
+TEST_NUM = 10000
+
+
+def extract_data(filenames):
+    # 验证文件是否存在
+    for f in filenames:
+        if not tf.gfile.Exists(f):
+            raise ValueError('Failed to find file: ' + f)
+    # 读取数据
+    labels = None
+    images = None
+
+    for f in filenames:
+        bytestream = open(f, 'rb')
+        # 读取数据
+        buf = bytestream.read(TRAIN_NUM * (IMAGE_SIZE * IMAGE_SIZE * IMAGE_DEPTH + LABEL_SIZE))
+        # 把数据流转化为np的数组
+        data = np.frombuffer(buf, dtype=np.uint8)
+        # 改变数据格式
+        data = data.reshape(TRAIN_NUM, LABEL_SIZE + IMAGE_SIZE * IMAGE_SIZE * IMAGE_DEPTH)
+        # 分割数组
+        labels_images = np.hsplit(data, [LABEL_SIZE])
+
+        label = labels_images[0].reshape(TRAIN_NUM)
+        image = labels_images[1].reshape(TRAIN_NUM, IMAGE_SIZE, IMAGE_SIZE, IMAGE_DEPTH)
+
+        if labels is None:
+            labels = label
+            images = image
+        else:
+            # 合并数组，不能用加法
+            labels = np.concatenate((labels, label))
+            images = np.concatenate((images, image))
+
+    # images = (images - (PIXEL_DEPTH / 2.0)) / PIXEL_DEPTH  此处无需归一化，此操作将放在计算图中
+
+    return labels, images
+
+
+def extract_train_data(files_dir, valid_size=0.1):
+    # 获得训练数据
+    filenames = [os.path.join(files_dir, 'data_batch_%d.bin' % i) for i in xrange(1, 6)]
+    labels, images =  extract_data(filenames)
+
+    indices = np.random.permutation(TRAIN_NUMS)
+    valid_idx, train_idx = indices[:TRAIN_NUMS * valid_size], indices[TRAIN_NUMS * valid_size:]
+
+    return images[train_idx], images[valid_idx], labels[train_idx], labels[valid_idx]
+
+
+def extract_test_data(files_dir):
+    # 获得测试数据
+    filenames = [os.path.join(files_dir, 'test_batch.bin'), ]
+    return extract_data(filenames)
+
+
+# 把稠密数据label[1,5...]变为[[0,1,0,0...],[...]...]
+def dense_to_one_hot(labels_dense, num_classes):
+    # 数据数量
+    num_labels = labels_dense.shape[0]
+    # 生成[0,1,2...]*10,[0,10,20...]
+    index_offset = np.arange(num_labels) * num_classes
+    # 初始化np的二维数组
+    labels_one_hot = np.zeros((num_labels, num_classes))
+    # 相对应位置赋值变为[[0,1,0,0...],[...]...]
+    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+
+    return labels_one_hot
+
+
+class Cifar10DataSet(object):
+    """docstring for Cifar10DataSet"""
+
+    def __init__(self, data_dir):
+        super(Cifar10DataSet, self).__init__()
+        self.train_images, self.valid_images, self.train_labels, self.valid_labels = extract_train_data(
+            os.path.join(data_dir, 'cifar10/cifar-10-batches-bin'))
+        self.test_labels, self.test_images = extract_test_data(os.path.join(data_dir, 'cifar10/cifar-10-batches-bin'))
+
+        print(self.train_labels.size)
+
+        self.train_labels = dense_to_one_hot(self.train_labels, NUM_CLASSES)
+        self.test_labels = dense_to_one_hot(self.test_labels, NUM_CLASSES)
+
+        # epoch完成次数
+        self.epochs_completed = 0
+        # 当前批次在epoch中进行的进度
+        self.index_in_epoch = 0
+
+    def next_train_batch(self, batch_size):
+        # 起始位置
+        start = self.index_in_epoch
+        self.index_in_epoch += batch_size
+        # print "self.index_in_epoch: ",self.index_in_epoch
+        # 完成了一次epoch
+        if self.index_in_epoch > TRAIN_NUMS:
+            # epoch完成次数加1
+            self.epochs_completed += 1
+            # print "self.epochs_completed: ",self.epochs_completed
+            # 打乱数据顺序，随机性
+            perm = np.arange(TRAIN_NUMS)
+            np.random.shuffle(perm)
+            self.train_images = self.train_images[perm]
+            self.train_labels = self.train_labels[perm]
+            start = 0
+            self.index_in_epoch = batch_size
+            # 条件不成立会报错
+            assert batch_size <= TRAIN_NUMS
+
+        end = self.index_in_epoch
+        # print "start,end: ",start,end
+
+        return self.train_images[start:end], self.train_labels[start:end]
+
+    def valid_data(self):
+        return self.valid_images, self.valid_labels
+
+    def test_data(self):
+        return self.test_images, self.test_labels
