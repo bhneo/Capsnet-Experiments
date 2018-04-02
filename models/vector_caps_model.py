18dc1298e86ca6a152884b582b570142962e016a
bhneo <bhneo@126.com>
2018-04-02 23:12:04 +0800

    update

--- models/vector_caps_model.py
+++ models/vector_caps_model.py
@@ -3,9 +3,9 @@ License: Apache-2.0
 Author: Huadong Liao
 E-mail: naturomics.liao@gmail.com
 """
+from models.base_model import BaseModel
 
 import tensorflow as tf
-from models.base_model import BaseModel
 import capslayer
 
 
@@ -34,7 +34,7 @@ class CapsNet(BaseModel):
         self.lambda_val = lambda_val
         self.reconstruction = reconstruction
         self.regularization_scale = regularization_scale
-        BaseModel.__init__(height, width, channels, num_label, is_training)
+        BaseModel.__init__(self, height, width, channels, num_label, is_training)
 
     def build_arch(self):
         with tf.variable_scope('Conv1_layer'):
@@ -43,27 +43,29 @@ class CapsNet(BaseModel):
 
         # return primaryCaps: [batch_size, 1152, 8, 1], activation: [batch_size, 1152]
         with tf.variable_scope('PrimaryCaps_layer'):
-            primaryCaps, activation = capslayer.layers.primaryCaps(conv1, filters=32, kernel_size=9, strides=2, out_caps_shape=[8, 1])
+            primaryCaps, activation = capslayer.layers.primaryCaps(conv1, filters=32, kernel_size=9, strides=2,
+                                                                   out_caps_shape=[8, 1])
 
         # return digitCaps: [batch_size, num_label, 16, 1], activation: [batch_size, num_label]
         with tf.variable_scope('DigitCaps_layer'):
             primaryCaps = tf.reshape(primaryCaps, shape=[self.batch_size, -1, 8, 1])
-            self.digitCaps, self.activation = capslayer.layers.fully_connected(primaryCaps, activation, num_outputs=10, out_caps_shape=[16, 1], routing_method='DynamicRouting')
+            self.digitCaps, self.activation = capslayer.layers.fully_connected(primaryCaps, activation, num_outputs=10,
+                                                                               out_caps_shape=[16, 1],
+                                                                               routing_method='DynamicRouting')
 
         if self.reconstruction:
             # Decoder structure in Fig. 2
             # Reconstructe the MNIST images with 3 FC layers
             # [batch_size, 1, 16, 1] => [batch_size, 16] => [batch_size, 512]
             with tf.variable_scope('Decoder'):
-                masked_caps = tf.multiply(self.digitCaps, tf.reshape(self.Y, (-1, self.num_label, 1, 1)))
+                masked_caps = tf.multiply(self.digitCaps, tf.reshape(self.one_hot_labels, (-1, self.num_label, 1, 1)))
                 active_caps = tf.reshape(masked_caps, shape=(self.batch_size, -1))
                 fc1 = tf.contrib.layers.fully_connected(active_caps, num_outputs=512)
                 fc2 = tf.contrib.layers.fully_connected(fc1, num_outputs=1024)
-                self.decoded = tf.layers.fully_connected(fc2, num_outputs=self.height * self.width * self.channels,
-                                                         activation_fn=tf.sigmoid)
+                self.decoded = tf.layers.dense(fc2, num_outputs=self.height * self.width * self.channels,
+                                               activation=tf.sigmoid)
 
         return self.activation
-
 
     def loss(self):
         # 1. Margin loss
@@ -99,7 +101,6 @@ class CapsNet(BaseModel):
             return self.margin_loss + self.regularization_scale * self.reconstruction_err
         else:
             return self.margin_loss
-
 
     # Summary
     def _summary(self):
